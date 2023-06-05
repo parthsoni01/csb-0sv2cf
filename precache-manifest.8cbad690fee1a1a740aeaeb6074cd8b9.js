c977ccaa54ca7c129ffb99cb48ea7d3b85846c4c
Code Sandbox <hello@codesandbox.io>
2023-06-05 10:07:18 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8cbad690fee1a1a740aeaeb6074cd8b9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "049d153eb4898f547139b4101dc7c7a5",
+    "url": "https://parthsoni01.github.io/csb-0sv2cf/index.html"
+  },
+  {
+    "revision": "bd37c975d9cc8eca7949",
+    "url": "https://parthsoni01.github.io/csb-0sv2cf/static/css/main.77565df6.chunk.css"
+  },
+  {
+    "revision": "35ebdf68fcc03def6a91",
+    "url": "https://parthsoni01.github.io/csb-0sv2cf/static/js/2.75a66fb9.chunk.js"
+  },
+  {
+    "revision": "bd37c975d9cc8eca7949",
+    "url": "https://parthsoni01.github.io/csb-0sv2cf/static/js/main.6beb96c0.chunk.js"
+  },
+  {
+    "revision": "c510631f1cc1cd3b09e3",
+    "url": "https://parthsoni01.github.io/csb-0sv2cf/static/js/runtime~main.153b81c8.js"
+  }
+]);
